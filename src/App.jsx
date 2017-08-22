061868ca6cdcfcf27c7dc82d810f2b725ce2f2d4
Hannah Kim <h.heeeun.kim@gmail.com>
2017-08-22 23:20:45 +0000

    utilized componentDidMount function to add a message and re-render all components

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,32 @@ class App extends Component {
 		  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
 		  messages: [
 			  {
-			  	id: 0,
+			  	id: 1,
 			  	username: "Bob",
 			  	content: "Has anyone seen my marbles?",
 			  },
 			  {
-			  	id: 1,
+			  	id: 2,
 			  	username: "Anonymous",
 			  	content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
 			  }
 			]
 		}
 	}
+
+	componentDidMount() {
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: messages})
+  		}, 3000);
+	}
+
 
 	render() {
 		console.log("Rendering <App/>");
