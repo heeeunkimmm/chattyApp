5de070a209abd8f2ba8d18d086b53dca3da804d2
Hannah Kim <h.heeeun.kim@gmail.com>
2017-08-22 19:33:17 +0000

    finished setting up react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
-  }
+	render() {
+		return (
+			<div>
+			<nav className="navbar">
+		    <a href="/" className="navbar-brand">Chatty</a>
+		  </nav>
+		  <MessageList />
+		  <ChatBar />
+		  </div>
+		);
+	}
 }
 export default App;
